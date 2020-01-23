720d75af0b0c77aaa45d9290cacfbaa8b6e9b589
Agnes Park <agnespark824@gmail.com>
2020-01-23 21:38:24 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,5 +1,7 @@
 array = ["a", "b", "c", "d"]
 
-def take_a_number(array, name)
-  if array.empty? 
+def take_a_number(array_one, name)
+  array_one << name
+  puts "Welcome, #{name}. You are number #{array_one.index(name) + 1} in line."
+end
   
