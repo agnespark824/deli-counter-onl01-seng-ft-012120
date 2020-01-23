e9b8644ba0b6ac351099d4ceffa26d797c711c8f
Agnes Park <agnespark824@gmail.com>
2020-01-23 21:38:51 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,7 +1,7 @@
-array = ["a", "b", "c", "d"]
+array = ["Ana", "Bob", "Cat"]
 
 def take_a_number(array_one, name)
   array_one << name
   puts "Welcome, #{name}. You are number #{array_one.index(name) + 1} in line."
 end
-  
+
